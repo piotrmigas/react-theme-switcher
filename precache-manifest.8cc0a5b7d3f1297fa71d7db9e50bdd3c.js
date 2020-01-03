36664fc6810ddb87cc86be3a0cd27a6bf437f625
piotrmigas <piotr.migas@icloud.com>
2020-01-03 15:19:22 +0100

    Updates

---
+++ precache-manifest.8cc0a5b7d3f1297fa71d7db9e50bdd3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76aa876171d0651a3c9af7086a0dfdee",
+    "url": "/react-theme-switcher/index.html"
+  },
+  {
+    "revision": "18f2d4c6b58dbe33bf78",
+    "url": "/react-theme-switcher/static/css/main.8c97af6d.chunk.css"
+  },
+  {
+    "revision": "54660c070903e394ed45",
+    "url": "/react-theme-switcher/static/js/2.dc01a365.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/react-theme-switcher/static/js/2.dc01a365.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18f2d4c6b58dbe33bf78",
+    "url": "/react-theme-switcher/static/js/main.11c56f4a.chunk.js"
+  },
+  {
+    "revision": "df8e3ef8114410d06039",
+    "url": "/react-theme-switcher/static/js/runtime-main.a6a75010.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-theme-switcher/static/media/logo.5d5d9eef.svg"
+  }
+]);
